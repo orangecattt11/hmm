76ad08a0a343d61c5ed7088460e0e7615d5922b3
Hilal Madyan <hilalmadyan4@gmail.com>
2023-07-11 12:03:24 +0700

    first commit

---
+++ sections/World.jsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { motion } from "framer-motion";
+
+import styles from "../styles";
+import { TitleText, TypingText } from "../components";
+import { fadeIn, staggerContainer } from "../utils/motion";
+
+const World = () => (
+  <section className={`${styles.paddings} relative z-10`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex flex-col`}
+    >
+      <TypingText title="a friend, the best friend" textStyles="text-center" />
+      <TitleText
+        title={
+          <>
+            "Through laughter and tears, adventures and misadventures, a best
+            friend stands as an unwavering pillar of support, making every
+            moment more meaningful"
+          </>
+        }
+        textStyles="text-center"
+      />
+
+      <motion.div
+        variants={fadeIn("up", "tween", 0.3, 1)}
+        className="relative mt-[68px] flex w-full h-full"
+      >
+          <img
+            src="/anisdinda2.jpg"
+            alt="foto anis dan dinda"
+            className=" object-cover w-full h-full rounded-[50px]"
+          />
+      </motion.div>
+    </motion.div>
+  </section>
+);
+
+export default World;
